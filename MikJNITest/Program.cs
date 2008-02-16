249e62480016117a2157c4c7a0a864f0edc2050d
Jean-Paul Mikkers <jean.paul.mikkers@gmail.com>
2008-02-16 20:37:11 +0000

    Removed dutch strings.

--- MikJNITest/Program.cs
+++ MikJNITest/Program.cs
@@ -16,19 +16,16 @@ namespace MikJNITest
 
             if( RawJavaVM.CreateJavaVM( out vm, out env ) == RawJNIResultCodes.OK )
             {
-                //RAWJNINativeInterface env;
                 RawJavaVMAttachArgs vmargs = new RawJavaVMAttachArgs();
                 vmargs.group = IntPtr.Zero;
                 vmargs.name = null;
                 vmargs.version = RawJNIVersion.VERSION_1_6;
-                //JNIDotNet.PJNIResultCodes.
 
                 Console.WriteLine("It's ok");
                 Console.WriteLine("Result of attachcurrentthread {0}", vm.AttachCurrentThread(out env, vmargs));
 
                 Rawjstring js = env.NewString("Test");
-                //env.DeleteLocalRef(new Rawjobject(js));
-                Console.WriteLine("inhoud van string {0}, lengte {1}, type {2}", env.GetStringChars(js), env.GetStringLength(js), env.GetObjectRefType( new Rawjobject( js ) ) );
+                Console.WriteLine("Contents of javastring: '{0}', length {1}, type {2}", env.GetStringChars(js), env.GetStringLength(js), env.GetObjectRefType( new Rawjobject(js) ) );
 
                 Rawjclass clinet = env.FindClass("java/net/InetAddress");
                 Console.WriteLine("class {0}", (IntPtr)clinet);
